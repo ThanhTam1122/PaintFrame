43d8bb0b49cd9885a0b6fa25bcc8f04378037ab2
kkyy0720 <kuriyuki0720@gmail.com>
2024-04-05 11:32:55 +0900

    feat: envとnextjsの設定を追加

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: '*',
+      },
+    ],
+  },
+  experimental: {
+    typedRoutes: false,
+  },
+};
 
 export default nextConfig;
