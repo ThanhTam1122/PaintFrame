917c6fc5ffa378ff6286dff5f86aec799309adf2
kkyy0720 <kuriyuki0720@gmail.com>
2024-04-05 10:45:36 +0900

    feat: 検索エンジン最適化のためのメタデータを追加する

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,5 +5,12 @@ export const cn = (...classes: ClassValue[]): string => {
   return twMerge(clsx(...classes));
 };
 
-export const isClient = typeof window !== 'undefined';
-export const isServer = typeof window === 'undefined';
+export const unique = <T, Key extends keyof T>(
+  array: Array<T> | T[],
+  property?: Key,
+): Array<T> => {
+  if (!property) return Array.from(new Set([...array]));
+
+  const set = new Set();
+  return array.filter((o: T) => !set.has(o[property]) && set.add(o[property]));
+};
